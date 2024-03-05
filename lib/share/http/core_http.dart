f12c500abb924ae89b312f1263b32e6eaaf42d4b
Filipe Haniel <filipe.haniel@gmail.com>
2024-03-05 19:37:28 -0300

    implements http core

---
+++ lib/share/http/core_http.dart
@@ -0,0 +1,16 @@
+import 'package:hero_flutter/share/http/core_response.dart';
+
+abstract class CoreHttp {
+  void addHeader(String key, String value);
+
+  Future<CoreResponse> get({required String route, dynamic queryParameters});
+
+  Future<CoreResponse> post(
+      {required String route, dynamic body, queryParameters});
+
+  Future<CoreResponse> put(
+      {required String route, dynamic body, queryParameters});
+
+  Future<CoreResponse> delete(
+      {required String route, dynamic body, queryParameters});
+}
