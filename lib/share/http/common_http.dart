c71a97bfbde98001b24eae00992cafaedf0e3a92
Filipe Haniel <filipe.haniel@gmail.com>
2024-03-12 18:03:46 -0300

    adding a common response

--- lib/share/http/common_http.dart
+++ lib/share/http/common_http.dart
@@ -1,1 +1,78 @@
-class CommonHttp {}
+import 'package:hero_flutter/share/http/common_response.dart';
+import 'package:hero_flutter/share/http/core_http.dart';
+
+import 'package:dio/dio.dart';
+
+class CommonHttp implements CoreHttp {
+  late Dio dio;
+
+  CommonHttp({required String baseUrl}) {
+    dio = Dio();
+    dio.options.baseUrl = baseUrl;
+  }
+
+  @override
+  void addHeader(String key, String value) {
+    dio.options.headers[key] = value;
+  }
+
+  @override
+  Future<CommonResponse> get({required String route, queryParameters}) async {
+    final response = await dio.get(
+      route,
+      queryParameters: queryParameters,
+    );
+
+    return CommonResponse(
+      statusCode: response.statusCode,
+      data: response.data,
+      headers: dio.options.headers,
+    );
+  }
+
+  @override
+  Future<CommonResponse> post(
+      {required String route, body, queryParameters}) async {
+    var response = await dio.post(
+      route,
+      data: body,
+    );
+
+    return CommonResponse(
+      statusCode: response.statusCode,
+      data: response.data,
+      headers: dio.options.headers,
+    );
+  }
+
+  @override
+  Future<CommonResponse> put(
+      {required String route, body, queryParameters}) async {
+    var response = await dio.post(
+      route,
+      data: body,
+    );
+
+    return CommonResponse(
+      statusCode: response.statusCode,
+      data: response.data,
+      headers: dio.options.headers,
+    );
+  }
+
+  @override
+  Future<CommonResponse> delete(
+      {required String route, body, queryParameters}) async {
+    var response = await dio.delete(
+      route,
+      data: body,
+      queryParameters: queryParameters,
+    );
+
+    return CommonResponse(
+      statusCode: response.statusCode,
+      data: response.data,
+      headers: dio.options.headers,
+    );
+  }
+}
---
+++ lib/share/http/common_response.dart
@@ -0,0 +1,16 @@
+import 'package:hero_flutter/share/http/core_response.dart';
+
+class CommonResponse implements CoreResponse {
+  @override
+  final int? statusCode;
+  @override
+  final dynamic data;
+  @override
+  final Map<String, dynamic> headers;
+
+  CommonResponse({
+    required this.statusCode,
+    required this.data,
+    required this.headers,
+  });
+}
